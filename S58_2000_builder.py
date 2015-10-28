1048313cfead4b255ff113e7dbd1024b3e41b1b0
MartinHvidberg <Martin@Hvidberg.net>
2015-10-28 16:11:05 +0100

    RC1

--- S58_2000_builder.py
+++ S58_2000_builder.py
@@ -13,6 +13,9 @@ bol_act = False # To act on this input line
 lst_att = list()
 lst_obt = list()
 dic_col = dict()
+bol_testcases = True
+bol_test = False
+bol_printall = True
 
 with open("S-58_2000_raw.txt") as f:
     for line in f:
@@ -22,79 +25,99 @@ with open("S-58_2000_raw.txt") as f:
             if len(lst_l) == 2: # Its an Attribute
                 str_att = lst_l[0]
                 lst_att.append(str_att)
-                #print "Att.="+str_att
             elif len(lst_l) > 2: # It must be a ObjType
                 str_obt = lst_l[0]
                 lst_obt.append(str_obt)
+                
                 # Test cases
-                lst_tcs = list()
-                bol_test = False
-                if str_obt == 'BUISGL' and str_att == 'COLOUR':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" * << Anything goes ..."
-                    
-                if str_obt == 'BOYSPP' and str_att == 'COLOUR':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" * # << Anything colour, but must have colour"
-                    
-                if str_obt == 'M_QUAL' and str_att == 'CATZOC':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" * (#) << Anything value, but must have value, and can't be unknown"
-                    
-                if str_obt == 'LIGHTS' and str_att == 'COLOUR':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" Numbers and # together << Mandetory, with colour restrictions"
-                    
-                if str_obt == 'FAIRWY' and str_att == 'RESTRN':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" many valus"
-                    
-                if str_obt == 'T_HMON' and str_att == 'T_MTOD':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" complex"
-                    
-                if str_obt == 'BRIDGE' and str_att == 'CONDTN':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" Numbers << Value restrictions"
-                    
-                if str_obt == 'CBLOHD' and str_att == 'CONDTN':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" Polluted info"
-                    
-                if str_obt == 'MORFAC' and str_att == 'WATLEV':
-                    bol_test = True
-                    print "\nTest case "+str_obt+" "+str_att+" Obsolete values"
-                    
-                # ------
-                if bol_test: print "raw: "+line.strip()
+                if bol_testcases:
+                    bol_test = False
+                    if str_obt == 'BUISGL' and str_att == 'COLOUR':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" * << Anything goes ..."
+                    if str_obt == 'BOYSPP' and str_att == 'COLOUR':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" * # << Anything colour, but must have colour"
+                    if str_obt == 'M_QUAL' and str_att == 'CATZOC':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" * (#) << Anything value, but must have value, and can't be unknown"
+                    if str_obt == 'LIGHTS' and str_att == 'COLOUR':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" Numbers and # together << Mandetory, with colour restrictions"
+                    if str_obt == 'FAIRWY' and str_att == 'RESTRN':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" many valus"
+                    if str_obt == 'T_HMON' and str_att == 'T_MTOD':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" complex"
+                    if str_obt == 'BRIDGE' and str_att == 'CONDTN':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" Numbers << Value restrictions"
+                    if str_obt == 'CBLOHD' and str_att == 'CONDTN':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" Polluted info"
+                    if str_obt == 'MORFAC' and str_att == 'WATLEV':
+                        bol_test = True
+                        print "\nTest case "+str_obt+" "+str_att+" Obsolete values"
+                    if bol_test: print "raw: "+line.strip()
+                    if bol_test: print "[2]: "+str(lst_l[2])
+                
                 if not str_obt in dic_col.keys(): # Make ObjType
                     dic_col[str_obt] = dict()
                 if not str_att in dic_col[str_obt].keys(): # Make Attrib
                     dic_col[str_obt][str_att] = dict()
-                if bol_test: print "[2]: "+str(lst_l[2])
                 if (RepresentsInt(lst_l[2])) or ("-" in lst_l[2]):
                     dic_col[str_obt][str_att]['Allow'] = lst_l[2].split('-') # Add Allowed
                 if ("#" in line):
                     dic_col[str_obt][str_att]['Manda'] = True # Add Allowed
                 if ("(#)" in line):
                     dic_col[str_obt][str_att]['Unknw'] = False # Add Allowed
+                    
                 if bol_test: print "dic: "+str(dic_col[str_obt][str_att])
+                
         if "====== Start from here =====" in line:
             bol_act = True
 
-# Print out
-if False:
-    lst_obt = list(set(lst_obt))
-    lst_obt.sort()
-    lst_att = list(set(lst_att))
-    lst_att.sort()
-    for obt in lst_obt:
-        for att in lst_att:
-            if att in dic_col[obt]:
-                if 'Manda' in dic_col[obt][att]:
+# Process to output
+lst_obt = list(set(lst_obt))
+lst_obt.sort()
+lst_att = list(set(lst_att))
+lst_att.sort()
+lst_rules = list()
+lst_rules.append("#: ruleID : ruleTitle : Mode : FC : FCsubtype : Condition : ActionType : Action : Comments")
+num_num = 0
+for obt in lst_obt:
+    for att in lst_att:
+        if att in dic_col[obt]:
+            
+            if 'Manda' in dic_col[obt][att]:
+                if bol_printall:
                     print obt+" : "+att+" : Mandatory "+str(dic_col[obt][att]['Manda'])
-                if 'Allow' in dic_col[obt][att]:
+                num_num += 1
+                str_human = "S-58 check 2000 says "+att+" is mandatory in "+obt
+                str_rule = ": "+str(num_num)+" : "+str_human+" : SQL : * : "+obt+" : "+att+" IS NULL : LOG : "
+                lst_rules.append(str_rule)
+                    
+            if 'Unknw' in dic_col[obt][att]:
+                if bol_printall:
+                    print obt+" : "+att+" : Unknw "+str(dic_col[obt][att]['Unknw'])
+                num_num += 1
+                str_human = "S-58 check 2000 says "+att+" can't be unknown in "+obt
+                str_rule = ": "+str(num_num)+" : "+str_human+" : SQL : * : "+obt+" : "+att+" <> '-32767' : LOG : "
+                lst_rules.append(str_rule)
+                    
+            if 'Allow' in dic_col[obt][att]:
+                if bol_printall:
                     print obt+" : "+att+" : "+str(dic_col[obt][att]['Allow'])
+                num_num += 1
+                str_human = "S-58 check 2000 says "+att+" must have one of these values on "+obt
+                list_of_values = str(dic_col[obt][att]['Allow']).replace('[','(').replace(']',')')
+                str_rule = ": "+str(num_num)+" : "+str_human+" : LOVE : * : "+obt+" : "+att+" % "+list_of_values+" : LOG : Alowed values "+list_of_values
+                lst_rules.append(str_rule)
 
-    print lst_obt
-    print lst_att
+with open('S-58_2000.gdbot','w') as f:
+    for str_rule in lst_rules:
+        f.write(str_rule+'\n')
+
+#    print lst_obt
+#    print lst_att
